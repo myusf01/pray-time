8bbb27e9efc19e1a0d3cee5de8fb82e063671a0d
Muhammed Yusuf <myusuf2121@gmail.com>
2022-03-12 13:12:52 +0300

    Update navbar.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <nav>
+  <nav class="flex justify-center space-x-4">
     <router-link to="/">Home</router-link>
-    <router-link to="/settings">Settings</router-link>
+    <router-link to="/settings">{{ userCity.name }}</router-link>
   </nav>
   <router-view />
 </template>
 
-<style></style>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'App',
+  computed: {
+    ...mapGetters(['userCity'])
+  }
+}
+</script>
