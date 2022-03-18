345a5b2dbe63d0de229302b8e10281f285c0a223
Muhammed Yusuf <myusuf2121@gmail.com>
2022-03-18 14:32:22 +0300

    Updated time styling.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <nav class="flex justify-center space-x-4">
-    <router-link to="/">Home</router-link>
-    <router-link to="/settings">{{ userCity }}</router-link>
-  </nav>
   <router-view />
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 export default {
-  name: 'App',
-  computed: {
-    ...mapGetters(['userCity'])
-  }
+  name: 'App'
 }
 </script>
--- src/components/Time.vue
+++ src/components/Time.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="time-card-container">
-    <div class="text-container">
-      <div class="active-time" v-if="isActiveTime">Time is active</div>
-      <p class="text-xl text-right">{{ timing }}</p>
-      <p class="text-right">{{ hour }}</p>
+  <div v-if="!isActiveTime" class="inactive-card-container">
+    <div class="text-container rotate-left inactive-text text-2xl">
+      <p>{{ timing }}</p>
+      <p class="font-bold">{{ hour }}</p>
+    </div>
+  </div>
+  <div v-else class="inactive-card-container active-card-container">
+    <div class="mx-auto space-y-10">
+      <router-link
+        class="font-bold active-text block mb-[30%] text-4xl"
+        to="/settings"
+        >{{ userCity }}</router-link
+      >
+      <div class="text-container active-text text-2xl">
+        <p>{{ timing }}</p>
+        <p class="font-bold">{{ hour }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'TimeComponent',
   props: { timing: String, hour: String, currentTime: String },
   computed: {
+    ...mapGetters(['userCity']),
     isActiveTime() {
       return this.timing === this.currentTime
     }
