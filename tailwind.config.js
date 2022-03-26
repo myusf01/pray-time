a76ce8b612df131ecf11839c8be4a717ded7877d
Muhammed Yusuf <myusuf2121@gmail.com>
2022-03-26 14:29:06 +0300

    Updated font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: { lora: ['Lora', 'sans'] }
+    }
   },
   plugins: []
 }
