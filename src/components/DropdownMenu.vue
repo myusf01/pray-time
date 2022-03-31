8f925a447e1aa225b20cdf7155fc2b8fc73fd959
Muhammed Yusuf <myusuf2121@gmail.com>
2022-03-31 15:08:58 +0300

    New components to create custom DropdownMenus.

---
+++ src/components/DropdownMenu.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    ref="dropdown"
+    class="dropdown-menu absolute no-scrollbar settings-text upper shadow-lg shadow-gray-400/50"
+  >
+    <span
+      class="block w-full rounded-lg text-stone-800 px-2 cursor-pointer hover:bg-gray-300/30"
+      v-for="item in menuArray"
+      @click="$emit('selectItem', item)"
+      :key="item.id"
+      :value="item"
+    >
+      {{ item.name }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DropdownMenu',
+  props: {
+    menuList: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      menuArray: this.menuList
+    }
+  },
+  methods: {
+    documentClick(e) {
+      // manage dropdown menu
+      // show or hide on click
+      try {
+        let el = this.$refs.dropdown
+        let target = e.target
+        // close menu when user clicked another content
+        if (el !== target && !el.contains(target)) {
+          this.$emit('close')
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  beforeMount() {
+    // add click event listener to trigger our method
+    document.addEventListener('click', this.documentClick)
+  },
+  beforeUnmount() {
+    // remove click event after use to prevent unpredicted errors
+    document.removeEventListener('click', this.documentClick)
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/SettingsMenu.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="relative w-full">
+    <button
+      @click.stop="isShow = !isShow"
+      :disabled="isDisabled"
+      :class="[{ 'bg-slate-300/50 text-slate-500': isDisabled }]"
+      class="settings-item pl-6 justify-between settings-text overflow-hidden bg-stone-100 text-stone-800 shadow-lg shadow-gray-300/50"
+    >
+      {{ menuLabel }}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        :class="[{ 'opacity-40': isDisabled }]"
+      >
+        <path fill="none" d="M0 0h24v24H0z" />
+        <path
+          d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z"
+        />
+      </svg>
+    </button>
+    <DropdownMenu
+      v-if="isShow"
+      :menuList="menuList"
+      @close="isShow = false"
+      @selectItem="
+        (e) => {
+          $emit('selectedItem', e)
+          isShow = false
+        }
+      "
+    />
+  </div>
+</template>
+
+<script>
+import DropdownMenu from './DropdownMenu.vue'
+export default {
+  name: 'SettingsMenu',
+  props: {
+    menuList: Array,
+    menuLabel: { type: String, default: 'Select' }
+  },
+  components: {
+    DropdownMenu
+  },
+
+  computed: {
+    isDisabled() {
+      return !this.menuList.length
+    }
+  },
+  data() {
+    return {
+      isShow: false,
+      menuTitle: this.menuLabel
+    }
+  },
+  methods: {
+    Title() {
+      if (this.isDisabled) {
+        this.menuTitle = ''
+      }
+      return this.menuTitle
+    }
+  }
+}
+</script>
